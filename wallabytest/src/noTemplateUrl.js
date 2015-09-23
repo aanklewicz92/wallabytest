f8187a9437cfad753f4483a77e88a9082d4a74f4
aanklewicz92 <aanklewicz92@gmail.com>
2015-09-23 13:23:23 +0200

    test

---
+++ wallabytest/src/noTemplateUrl.js
@@ -0,0 +1,26 @@
+﻿(function (module) {
+    var pxNoTemplateUrlController = function ($log, $scope) {
+        var vm = this;
+        vm.showAlert = false;
+        vm.onButtonClick = function () {
+            vm.showAlert = true;
+            $log.log('Clicked test');
+        }
+    };
+
+    var pxNoTemplateUrl = function () {
+        return {
+            restrict: 'E',
+            scope: {
+                callback: '&',
+                msgTooltip: '=',
+                isDisabled: '='
+            },
+            template: '<a ng-click="vm.onButtonClick()">Click</a>',
+            controller: ['$log', '$scope', pxNoTemplateUrlController],
+            controllerAs: 'vm',
+            replace: true
+        };
+    }
+    module.directive('pxNoTemplateUrl', pxNoTemplateUrl);
+}(angular.module('myproject.procedures')));
